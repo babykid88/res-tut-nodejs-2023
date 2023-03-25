be4f7cc9b6a35325a21f47efd3f330df5995e960
babykid88 <incredibletbf@gmail.com>
2023-03-25 16:34:36 +0800

    updated post.js

--- routes/post.js
+++ routes/post.js
@@ -2,26 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.post('/', (req,res) =>{
+router.post('/', async (req,res) =>{
     const post = new Post({
         title: req.body.title,
         description: req.body.description,
         name: req.body.name
     });
-
-    post.save()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
+    try {
+    const savedPost = await post.save();
+        res.json(savedPost);
+    } catch(err) {
             res.json({message: err});
-        });
-
+        }    
 });
 
-
-router.get('/', (req,res)=>{
-    res.send('we are on posts');
+router.get('/', async (req,res)=>{
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    } catch(err){
+        res.json({message:err});
+    }
 });
 
 router.get('/specific', (req,res)=>{
